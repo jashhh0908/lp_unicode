ee6b9a0e58a5f543a9a072bd9888f617bd88d327
jashhh0908 <jashunik9@gmail.com>
2025-10-05 17:14:45 +0530

    feat: add update document controller and route

--- controllers/docController.js
+++ controllers/docController.js
@@ -42,7 +42,36 @@ const getDocument = async (req, res, next) => {
         next(error);
     }
 }
+
+const updateDocument = async (req, res, next) => {
+    try {
+        const docID = req.params.id;
+        const userID = req.user.id;
+        const document = await DocumentModel.findById(docID);
+        if(!document)
+            return res.status(404).json({ error: "Document not found" });
+        if(!document.access.edit.includes(userID))
+            return res.status(403).json({ error: "You don't have permission to edit this document" });
+
+        const updatedFields = {
+            title: req.body.title,
+            content: req.body.content
+        }
+        const updatedDoc = await DocumentModel.findByIdAndUpdate( docID, 
+            updatedFields,
+            { new: true, overwrite: true }
+        )
+
+        res.status(200).json({ 
+            message: "Document updated successfully",
+            document: updatedDoc
+        })
+    } catch (error) {
+        next(error);
+    }
+}
 export {
     createDocument,
     getDocument,
+    updateDocument
 }
--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-import { createDocument, getDocument } from '../controllers/docController.js';
+import { createDocument, getDocument, updateDocument } from '../controllers/docController.js';
 const router = express.Router();
 
 router.post('/create', authMiddleware, createDocument);
 router.get('/read', authMiddleware, getDocument);
+router.patch('/update/:id', authMiddleware, updateDocument);
 
 export default router;
