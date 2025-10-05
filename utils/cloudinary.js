c92073bd9c38cb9656fb6cbcd576dcf24c218c58
jashhh0908 <jashunik9@gmail.com>
2025-10-05 10:27:47 +0530

    feat: setup Cloudinary configuration

---
+++ utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (filePath) => {
+    try {
+        if(!filePath) throw new Error("File path is required");
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto",
+        })
+        console.log("File uploaded successfully ", response.url);
+        fs.unlinkSync(filePath); // remove the locally saved temporary file after successful upload
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath); // remove the locally saved temporary file as the upload failed
+        return null;
+    }
+}
+
+export default uploadToCloudinary;
