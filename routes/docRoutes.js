6c224bded497fdab33d9520847156e974f6e0468
jashhh0908 <jashunik9@gmail.com>
2025-11-02 19:22:57 +0530

    connect get history function to route

--- routes/docRoutes.js
+++ routes/docRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-import { createDocument, getDocument, updateDocument, deleteDocument, requestAccess, approveRequest, addUserAccess } from '../controllers/docController.js';
+import { createDocument, getDocument, updateDocument, deleteDocument, requestAccess, approveRequest, addUserAccess, getDocHistory } from '../controllers/docController.js';
 const router = express.Router();
 
 router.post('/create', authMiddleware, createDocument);
@@ -10,4 +10,5 @@ router.delete('/delete/:id', authMiddleware, deleteDocument);
 router.post('/request-access/:id', authMiddleware, requestAccess);
 router.patch('/approve-request/:id', authMiddleware, approveRequest);
 router.patch('/add-user-access/:id', authMiddleware, addUserAccess);
+router.get('/:id/history', authMiddleware, getDocHistory)
 export default router;
