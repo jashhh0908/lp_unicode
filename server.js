6e902b2fe4368706064b8ed26c37ba385385487a
jashhh0908 <jashunik9@gmail.com>
2025-11-26 12:02:34 +0530

    added basic socket.io connection

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
+import { Server } from "socket.io";
+import path from "path";
+import http from "http";
 
 import logger from "./config/logger.js";
 import connectDB from "./config/db.js";
@@ -11,7 +13,8 @@ import profileRoute from "./routes/profileRoutes.js";
 import docRoute from "./routes/docRoutes.js";
 
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 dotenv.config();
@@ -23,6 +26,13 @@ app.use(express.json());
 if (NODE_ENV == 'development'){
   app.use(morgan("tiny"));
 }
+
+//socket.io
+io.on("connection", (socket) => {
+  console.log("A new user has connected", socket.id);
+});
+
+app.use(express.static(path.resolve("./public")));
 
 //global error handler
 app.use((err, req, res, next) => {
@@ -36,7 +46,8 @@ app.use('/document', docRoute);
 
 async function startServer() {
   await connectDB();   
-  app.listen(PORT, () => {
+
+  server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
   });
 };
