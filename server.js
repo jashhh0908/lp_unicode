372536c7e64faef2302cc69b56642ef8522b3b23
Jash Nikumbhe <jashunik9@gmail.com>
2026-03-26 15:58:47 +0530

    separate socket logic from server

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import siteRoute from "./routes/siteRoutes.js";
 import profileRoute from "./routes/profileRoutes.js";
 import docRoute from "./routes/docRoutes.js";
 import chatbotRoute from './routes/chatbotRoutes.js';
+import { useSocket } from "./socket/socket.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -20,127 +21,8 @@ const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 dotenv.config();
 
-
-
 //socket.io
-
-const presenceMap = new Map();  
-function emitPresence(documentId) {
-    const docSessions = presenceMap.get(documentId);
-    let users;
-    if(docSessions) {
-        users = [...docSessions.entries()].map(([clientId, data]) => ({
-            clientId,
-            userId: data.userId,
-            lastActive: data.lastActive
-        }))
-    } else {
-        users = [];
-    }
-
-    io.to(documentId).emit("presence:update", {
-        documentId,
-        users
-    });
-}
-
-io.on("connection", (socket) => {
-    console.log("A new user has connected", socket.id);
-    socket.on("joinDoc", ({documentId, userId}) => {
-        if(!documentId || !userId)
-            return;
-        socket.join(documentId);
-        
-        if(!presenceMap.has(documentId))
-            presenceMap.set(documentId, new Map());
-        
-        presenceMap.get(documentId).set(socket.id, {
-            userId,
-            lastActive: Date.now()
-        });
-
-        console.log(`Socket ${socket.id} joined document: ${documentId} as ${userId}`);   
-        emitPresence(documentId);
-    });
-    socket.on("doc:change", ({ documentId, content }) => {
-        if (!documentId) return;
-
-        // send updated content to everyone else in the room
-        socket.to(documentId).emit("doc:update", {
-            content
-        });
-    });
-    socket.on("heartbeat", ({documentId}) => {
-        const docSessions = presenceMap.get(documentId);
-        if(!docSessions) return;
-            const session = docSessions.get(socket.id);
-        if(session) {
-            session.lastActive = Date.now();
-        //console.log(`Heartbeat from ${socket.id} for ${documentId}`); (testing purpose only)
-        }
-    });
-
-    socket.on("leaveDoc", ({documentId}) => {
-        const docSessions = presenceMap.get(documentId);
-        if(!docSessions) return;
-        if(docSessions.delete(socket.id)) //.delete returns true/false depending on if the doc is removed or not
-            console.log(`Socket ${socket.id} left document: ${documentId}`);
-        socket.leave(documentId)
-        emitPresence(documentId); //update the users still left in the room
-        
-        //update UI of the user who currently left showing active people except them
-        let users = [];
-        docSessions.forEach((data, clientId) => {
-            users.push({
-                clientId: clientId,
-                userId: data.userId,
-                lastActive: data.lastActive
-            });
-        });
-        socket.emit("presence:update", {
-            documentId: documentId,
-            users: users
-        })
-    });
-
-    socket.on("disconnect", ({documentId}) => {
-        console.log(`Socket ${socket.id} disconnected`);
-        for(const [docId, docSessions] of presenceMap.entries()) { 
-          /*
-            map uses key, value structure => (docId, sessionData)
-            hence to destructure we do [docId, docSessions]
-          */
-            if(docSessions.delete(socket.id)) {
-                console.log(`Removed ${socket.id} from document ${docId}`);
-                emitPresence(docId);
-            }
-
-            if(docSessions.size === 0) //if the document room becomes empty on deletion then we delete it 
-                presenceMap.delete(docId);
-        }
-    });
-});
-
-//remove session if there is inactivity
-setInterval(() => {
-    const currentTime = Date.now();
-    const timeout = NODE_ENV === 'development' ? 30*1000 : 5*60*1000;
-    for(const [docId, docSessions] of presenceMap.entries()) {
-        let changed = false;
-        for(const [clientId, data] of docSessions.entries()) {
-            if(currentTime - data.lastActive > timeout) {
-            docSessions.delete(clientId);
-            changed = true;
-            console.log(`Removed client ${clientId} from document ${docId} due to inactivity.`);
-            }
-        }
-        if(changed)
-            emitPresence(docId);
-        if(docSessions.size === 0) {
-            presenceMap.delete(docId);
-        }
-    }
-}, 30000);
+useSocket(io);
 
 //middleware
 app.use(cookieParser());
