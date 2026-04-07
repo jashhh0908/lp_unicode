c09e3fbe77c8176cf406ef24705f01cef6052b1c
Jash Nikumbhe <jashunik9@gmail.com>
2026-04-07 09:36:59 +0530

    implement axios interceptors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,4 +5,49 @@ const api = axios.create({
     withCredentials: true
 });
 
+api.interceptors.request.use((config) => {
+    const savedUser = localStorage.getItem('user');
+    
+    if (savedUser) {
+        const parsedData = JSON.parse(savedUser);
+        if (parsedData.token) {
+            config.headers.Authorization = `Bearer ${parsedData.token}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        
+        if (error.response && error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true; // Mark it so we don't end up in an infinite loop!
+            
+            try {
+                const res = await api.post('/refresh'); 
+                
+                const savedUser = JSON.parse(localStorage.getItem('user'));
+                savedUser.token = res.data.token;
+                localStorage.setItem('user', JSON.stringify(savedUser));
+                
+                originalRequest.headers.Authorization = `Bearer ${res.data.token}`;
+                return api(originalRequest);
+                
+            } catch (refreshError) {
+                console.error("Refresh token expired. Hard logout required.");
+                localStorage.removeItem('user');
+                window.location.href = '/login'; 
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api;
