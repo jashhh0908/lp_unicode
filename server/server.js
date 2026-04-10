4a921404c9021900513dc22a57d08b7891731bee
Jash Nikumbhe <jashunik9@gmail.com>
2026-04-10 17:49:50 +0530

    update CORS origin for client side

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
         methods: ["GET", "POST", "PUT", "PATCH"],
         credentials: true
     }
